a7f7136e6004bc1619350f4547f72649f3274422
zYahiir <143060323+zYahiir@users.noreply.github.com>
2024-02-15 13:39:07 -0600

    Add files via upload

---
+++ CajaTextoAnt/src/logica/CajaTextoAnt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import gui.Ventana;
+import gui.VentanaPrincipal;
+
+/**
+ *
+ * @author PILARES
+ */
+public class CajaTextoAnt {
+    public static void main(String []args){
+        VentanaPrincipal pantalla1 = new VentanaPrincipal();
+        pantalla1.setVisible(true);
+        pantalla1.setResizable(false);
+        pantalla1.setLocation(null);
+        pantalla1.setTitle("Ventana");
+    }
+    
+}
